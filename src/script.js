468fac5114649ce09f782dfff76bd3f7b65fd8c5
eriksand <er.sandstrom@gmail.com>
2015-05-13 16:45:53 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -115,7 +115,7 @@ var game = { // a container for all relevant GAME information
                 && mouseX > helper.x && mouseX < helper.x + helper.enemyWidth) {
                     
                 helper.hitPoints--;
-                playOuchSound();
+                game.playOuchSound();
             }
         });
     },
