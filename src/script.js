b6fcb7fd2a5a64af5ed05ea1f9fcf85bff371c2f
eriksand <er.sandstrom@gmail.com>
2015-06-02 14:32:19 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -352,7 +352,7 @@ var game = { // a container
         /*
         Initialize the game
         */
-        game.setBackground();
+        //game.setBackground();
         game.canvas = document.querySelector("canvas"); //assign canvas
         game.canvas.context = game.canvas.getContext("2d"); //assign context
         game.elements.enemies = []; //init array
