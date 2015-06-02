e39f7eaab6c9c9c23d46498368a65eaadd3994bd
eriksand <er.sandstrom@gmail.com>
2015-06-02 16:50:37 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -357,7 +357,7 @@ var game = { // a container
         Initialize the game
         */
         game.background = document.querySelector("canvas"); //assign canvas to background
-        game.canvas.context = game.canvas.getContext("2d"); //assign context
+        game.background.context = game.canvas.getContext("2d"); //assign context
         game.setBackground();        
         game.canvas = document.querySelector("canvas"); //assign canvas
         game.canvas.context = game.canvas.getContext("2d"); //assign context
