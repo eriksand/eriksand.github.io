c5309abbbc0c586badc22a16daff38a2ce9245e1
eriksand <er.sandstrom@gmail.com>
2015-11-11 15:44:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var canvas = document.getElementById("myChart");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+window.onload = function{
+  var canvas = document.getElementById("myChart");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,150,75);
+}
+
