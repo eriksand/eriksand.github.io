d640fd944c7728b2456175210c1b17ef1c756de6
eriksand <er.sandstrom@gmail.com>
2015-11-11 15:19:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("myChart");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+var myBarChart = new Chart(ctx).Bar(data, options);
+
+var data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+
+  datasets: [
+    {
+      label: "My First dataset",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+ 
+var options = {};
