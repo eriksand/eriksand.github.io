54ef2920b7c0204766fc2202527169646dc18a88
eriksand <er.sandstrom@gmail.com>
2015-11-11 15:39:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,3 @@ var canvas = document.getElementById("myChart");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,75);
-
-var data = [
-    {
-        value: 20,
-        color:"#637b85"
-    },
-    {
-        value : 30,
-        color : "#2c9c69"
-    },
-    {
-        value : 40,
-        color : "#dbba34"
-    },
-    {
-        value : 10,
-        color : "#c62f29"
-    }
-
-];
-
-// For a pie chart
-var myPieChart = new Chart(ctx[0]).Pie(data);
-
